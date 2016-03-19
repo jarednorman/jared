36f8619cca2a7522d426a9696b93a2dde0a587f9
Jared Norman <jared@creepywizard.com>
2016-03-19 12:57:39 -0700

    Initialize curses stuff

--- lib/jared.rb
+++ lib/jared.rb
@@ -1,13 +1,39 @@
+require "curses"
 require "jared/version"
 
 module JarEd
   class << self
     def start(args)
-      puts args.inspect
+      with_screen do
+        loop do
+          stop! if Curses.getch == 'q'
+          Curses.doupdate
+          break if stop
+        end
+      end
     end
 
     def norman
       "Jared Norman"
+    end
+
+    def stop!
+      @stop = true
+    end
+
+    private
+
+    attr_reader :stop
+
+    def with_screen
+      Curses.init_screen
+      begin
+        Curses.crmode
+        Curses.noecho
+        yield
+      ensure
+        Curses.close_screen
+      end
     end
   end
 end
